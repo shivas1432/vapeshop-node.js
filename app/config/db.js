15487d9e5d20fff30ee749f555ffaf42dec0efd4
shivashanker <shivashanker7337@gmail.com>
2024-12-09 13:43:56 +0000

    Updated db.js to handle new MySQL connection configuration

--- app/config/db.js
+++ app/config/db.js
@@ -1,16 +1,34 @@
 const mysql = require('mysql2');
 require('dotenv').config(); // Load environment variables
+const url = require('url');
 
-// Create a MySQL connection pool using local environment variables
+// Check if DB_URL is provided
+const dbConfig = process.env.DB_URL
+  ? (() => {
+      const { hostname, port, auth, pathname } = new URL(process.env.DB_URL);
+      const [user, password] = auth.split(':');
+      return {
+        host: hostname,
+        port: port || 3306,
+        user: user,
+        password: password,
+        database: pathname.replace('/', ''), // Remove leading slash
+      };
+    })()
+  : {
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'your_database',
+      port: process.env.DB_PORT || 3306,
+    };
+
+// Create a MySQL connection pool
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'your_database',
-    connectionLimit: 10, // Adjust the pool size as needed
+  ...dbConfig,
+  connectionLimit: 10, // Adjust the pool size as needed
 });
 
 // Export a promise-based version of the pool to make queries easier
 const promisePool = pool.promise();
-
 module.exports = promisePool;
